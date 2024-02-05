ac9601d3c4ef855a24bcbd11d47f7b6c0218961a
leoblautzik <leoblautzik@gmail.com>
2024-02-05 13:33:18 -0300

    Con division y resto

---
+++ recursivas/division/division.go
@@ -0,0 +1,17 @@
+package division
+
+func Cociente(a, b int) int {
+	cociente := 1
+	if a-b < 0 {
+		return 0
+	}
+	return cociente + Cociente(a-b, b)
+}
+
+func Resto(a, b int) int {
+	resto := 0
+	if a-b < 0 {
+		return a
+	}
+	return resto + Resto(a-b, b)
+}
